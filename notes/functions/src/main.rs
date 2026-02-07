6867a957210762e947af5a49363b469fbe4886b5
Peter <petertannisiii@gmail.com>
2026-02-06 22:36:59 -0500

    finished 3.3

--- notes/functions/src/main.rs
+++ notes/functions/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
     println!("Hello, world!");
-
-    another_function(5);
+    let val = y();
+    another_function(val, 'k');
 }
 
 // looks like rust does not care if my function
 // is below where it is called
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn another_function(x: i32, unit_label: char) {
+    println!("The value of x is: {x}{unit_label}");
+}
+
+fn y() -> i32{
+        let num = 4;
+        num + 1
+            // the missing ; makes it an expression
+            // instead of a statement so it returns 5
 }
